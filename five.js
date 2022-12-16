d1ad818aecf5b4db970400c2f2fab713160c9d97
adam-stover <nehtaro@gmail.com>
2022-12-16 14:35:11 -0800

    five part one

---
+++ five.js
@@ -0,0 +1,33 @@
+import { getLines } from './helpers.js';
+
+const URL = './inputs/five.txt';
+
+export default async function dayFive() {
+  const lines = await getLines(URL);
+  const stackLines = lines.slice(0, 8);
+  const instructions = lines.slice(10);
+  const stacks = [];
+
+  for (let i = 0; i < 9; ++i) stacks.push([]);
+
+  for (let i = stackLines.length - 1; i >= 0; --i) {
+    const line = stackLines[i];
+    for (let j = 0; j < stacks.length; j++) {
+      const item = line[1 + j * 4];
+      if (item !== ' ') stacks[j].push(item);
+    }
+  }
+
+  for (let i = 0; i < instructions.length; ++i) {
+    const [_, count, __, originIndex, ___, destinationIndex] = instructions[i].split(' ').map(Number);
+    const origin = stacks[originIndex - 1];
+    const destination = stacks[destinationIndex - 1];
+
+    for (let j = 0; j < count; ++j) {
+      destination.push(origin.pop());
+    }
+  }
+
+  const topCrates = stacks.map(stack => stack[stack.length - 1]).join('');
+  console.log(topCrates);
+}
--- index.js
+++ index.js
@@ -2,8 +2,10 @@ import dayOne from './one.js';
 import dayTwo from './two.js';
 import dayThree from './three.js';
 import dayFour from './four.js';
+import dayFive from './five.js';
 
 // dayOne();
 // dayTwo();
 // dayThree();
-dayFour();
+// dayFour();
+dayFive();
