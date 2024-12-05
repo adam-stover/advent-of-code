3d4dd6640bf677824c93bb99924115d4bbbd203e
adam-stover <nehtaro@gmail.com>
2024-12-04 21:21:44 -0800

    day 5

--- solutions/5.js
+++ solutions/5.js
@@ -1,7 +1,7 @@
 import { getLines, ints, filterMap, has, max, min, copyExcept, makeMatrix, log } from '../utils.js';
 
 let URL = './inputs/5.txt';
-URL = './inputs/t.txt';
+// URL = './inputs/t.txt';
 
 const helper = (a, b, c) => {
   return false;
@@ -10,24 +10,63 @@ const helper = (a, b, c) => {
 export async function run() {
   const lines = await getLines(URL);
 
-  let count = 0;
+  const split = lines.indexOf('');
+  const ruleLines = lines.slice(0, split).map(ints);
+  const updates = lines.slice(split + 1).map(ints);
+
+  const ruleMap = {};
+
+  for (const [a, b] of ruleLines) {
+    if (!has(ruleMap, a)) {
+      ruleMap[a] = {
+        prev: new Set(),
+        next: new Set(),
+      }
+    }
+    if (!has(ruleMap, b)) {
+      ruleMap[b] = {
+        prev: new Set(),
+        next: new Set()
+      }
+    }
+
+    ruleMap[a].next.add(b);
+    ruleMap[b].prev.add(a);
+  }
+
   let sum = 0;
 
-  for (let i = 0; i < lines.length; i++) {
-    const line = lines[i];
-    const nums = ints(line);
+  const sortFn = (a, b) => {
+    if (ruleMap[a].next.has(b)) return -1;
+    if (ruleMap[a].prev.has(b)) return 1;
 
-    const cond = helper(nums);
+    return 0;
+  }
 
-    if (cond) {
-      const num = 0;
-      count++;
-      sum += num;
+  const isUpdateSorted = (update) => {
+    for (let i = 1; i < update.length; i++) {
+      if (sortFn(update[i - 1], update[i]) === 1) return false;
+    }
+
+    return true;
+
+    // const sorted = [...update].sort(sortFn);
+    // return (update.join('|') === sorted.join('|'));
+  }
+
+  for (const update of updates) {
+    if (!isUpdateSorted(update)) {
+      const sorted = [...update].sort(sortFn);
+      const medianIndex = Math.floor(sorted.length / 2);
+      // log(update)
+      // log(update[medianIndex])
+      sum += sorted[medianIndex];
     }
   }
 
-  log(count);
   log(sum);
+
+  // Not 5238
 }
 
 export default run;
