35a710d9f42fd49ce23150b6b267bbadbe0cc41d
Adam Stover <nehtaro@gmail.com>
2024-12-18 15:28:24 -0800

    Day 1 in Rust!

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,38 @@
+use std::collections::HashSet;
+
+const INPUT_TEXT: &str = include_str!("../../inputs/1.txt");
+
 fn main() {
-    println!("Hello, world!");
+    let mut first: Vec<usize> = Vec::new();
+    let mut second: Vec<usize> = Vec::new();
+    let mut sum: usize = 0;
+    let mut sum_2: usize = 0;
+    let mut hash: HashSet<usize> = HashSet::new();
+
+    for line in INPUT_TEXT.lines() {
+        let mut iter = line.split_whitespace().map(|x| x.parse::<usize>().unwrap());
+        let first_val = iter.next().unwrap();
+        let second_val = iter.next().unwrap();
+        first.push(first_val);
+        second.push(second_val);
+        hash.insert(first_val);
+    }
+
+    first.sort();
+    second.sort();
+
+    for (i, a) in first.iter().enumerate() {
+        let b: &usize = &second[i];
+
+        sum += if a > b { a - b } else { b - a };
+    }
+
+    for num in second.iter() {
+        if hash.contains(num) {
+            sum_2 += num;
+        }
+    }
+
+    println!("{sum}");
+    println!("{sum_2}");
 }
