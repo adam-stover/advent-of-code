d63ee44f1083811a0dbc89455ac0ed85cc0bfd37
Adam Stover <nehtaro@gmail.com>
2024-12-22 23:53:51 -0800

    added recursive solution -- iterative still faster

--- solutions/23.js
+++ solutions/23.js
@@ -6,6 +6,7 @@ let URL = './inputs/23.txt';
 export async function run() {
   const lines = await getLines(URL);
 
+  console.time('eyo');
   const nodes = new Set();
   const tNodes = new Set();
   const edges = {};
@@ -22,79 +23,39 @@ export async function run() {
     if (b.startsWith('t')) tNodes.add(b);
   }
 
-  const collectGroup = (node) => {
-    const seen = new Set();
+  const lehNodes = new Map();
 
-    const queue = [node];
-
-    while (queue.length) {
-        const cur = queue.shift();
-        if (seen.has(cur)) continue;
-        seen.add(cur);
-        for (const nextNode of edges[cur]) {
-            queue.push(nextNode);
-        }
-    }
-
-    return seen;
+  for (const key of nodes.keys()) {
+    lehNodes.set(key, { key, edges: edges[key] });
   }
 
-  const getGroups = (set) => {
-    const groups = new Set();
-    for (const node of set.keys()) {
-        const nextNodes = edges[node];
-        for (const nextNode of nextNodes.keys()) {
-            const nextNodeNodes = edges[nextNode];
-            for (const nextNextNode of nextNodeNodes.keys()) {
-                // log(`We are evaluating ${nextNextNode} which is edging ${nextNode} which is edging ${tNode}`)
-                if (nextNodes.has(nextNextNode)) {
-                    const key = [node, nextNode, nextNextNode]
-                        .sort((a, b) => a.localeCompare(b))
-                        .join('-');
-                    groups.add(key)
-                }
-            }
-        }
-      }
+  const getGroupsRecurse = (node, required = [], _refs = new WeakSet()) => {
+    if (_refs.has(node)) return required;
+    _refs.add(node);
+    const nextNodes = node.edges;
 
-      return groups;
-  }
-
-  const memo = (() => {
-    const cache = {};
-
-    return (node, required = []) => {
-        const key = `${node}|${required.join('-')}`;
-
-        if (!has(cache, key)) cache[key] = getGroupsRecurse(node, required);
-        return cache[key];
-    }
-  })();
-
-  const getGroupsRecurse = (node, required = []) => {
-    // log(required);
-    const nextNodes = edges[node];
-
-    if (required.every(node => nextNodes.has(node))) {
-        const nextRequirement = [...required, node];
+    if (required.every(key => nextNodes.has(key))) {
+        const nextRequirement = [...required, node.key];
         const requirements = filterMap(
-            [...nextNodes],
+            [...nextNodes].map(key => lehNodes.get(key)),
             n => !required.includes(n),
-            n => memo(n, nextRequirement)
+            n => getGroupsRecurse(n, nextRequirement, _refs)
         );
         const bestRequirement = max(requirements, r => r.length);
+        _refs.delete(node.key);
         return bestRequirement
     }
-
+    _refs.delete(node);
     return required;
   }
 
-  const doThing = (startingNode) => {
+  const getGroups = (startingNode) => {
     const seen = new Set();
     const queue = [startingNode];
 
     while (queue.length) {
       const node = queue.pop();
+      if (!seen.has(node) && edges[node].isSupersetOf)
       if (seen.has(node)) continue;
       const nextNodes = edges[node];
       if (nextNodes.isSupersetOf(seen)) {
@@ -108,40 +69,19 @@ export async function run() {
 
   let best = [];
 
+  // for (const [key, node] of lehNodes.entries()) {
+  //   const reqs = getGroupsRecurse(node);
+  //   if (reqs.length > best.length) best = reqs;
+  // }
+
   for (const key of nodes.keys()) {
-    const group = doThing(key);
+    const group = getGroups(key);
     if (group.length > best.length) best = group;
   }
 
+  console.timeEnd('eyo');
+
   log(best.sort((a, b) => a.localeCompare(b)).join(','));
-
-  // let best = [];
-
-  // for (const key of nodes.keys()) {
-  //   const cur = memo(key);
-  //   log(`${key} gives us ${cur}`)
-  //   if (cur.length > best.length) best = cur;
-  // }
-
-  // log(best.sort((a, b) => a.localeCompare(b)).join(','));
-
-//   const groups = getGroups(nodes);
-//   log(groups.size);
-
-//   const tNodeGroups = getGroups(tNodes);
-//   log(tNodeGroups.size)
-
-//   let count = 0;
-
-//   for (const group of groups) {
-//     const computers = group.split('-');
-//     if (computers.some(c => c.startsWith('t'))) {
-//         // log(computers);
-//         count++;
-//     }
-//   }
-
-//   log(count);
 }
 
 export default run;
