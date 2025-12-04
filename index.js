9fd313b8e76a1637d007e7d1a48e48a2ceb737fd
Adam Stover <nehtaro@gmail.com>
2025-12-03 21:22:14 -0800

    2025 day 4

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import dayOne from './solutions/1.js';
 import dayTwo from './solutions/2.js';
 import dayThree from './solutions/3.js';
-// import dayFour from './solutions/4.js';
+import dayFour from './solutions/4.js';
 // import dayFive from './solutions/5.js';
 // import daySix from './solutions/6.js';
 // import daySeven from './solutions/7.js';
@@ -26,8 +26,8 @@ import dayThree from './solutions/3.js';
 
 // dayOne();
 // dayTwo();
-dayThree();
-// dayFour();
+// dayThree();
+dayFour();
 // dayFive();
 // daySix();
 // daySeven();
---
+++ solutions/4.js
@@ -0,0 +1,57 @@
+import { getLines, ints, filterMap, has, max, min, copyExcept, makeMatrix, log } from '../utils.js';
+
+let URL = './inputs/4.txt';
+// URL = './inputs/t.txt';
+
+const PAPER = '@';
+const AIR = '.';
+
+export async function run() {
+  let lines = await getLines(URL);
+  const HEIGHT = lines.length;
+  const WIDTH = lines[0].length;
+
+  let count = 0;
+
+  const countAdjacent = (i, j) => {
+    let num = 0;
+    if (i !== 0) {
+      if (j !== 0 && lines[i - 1][j - 1] === PAPER) num++;
+      if (lines[i - 1][j] === PAPER) num++;
+      if (j < WIDTH && lines[i - 1][j + 1] === PAPER) num++;
+    }
+    if (j !== 0 && lines[i][j - 1] === PAPER) num++;
+    if (j < WIDTH && lines[i][j + 1] === PAPER) num++;
+    if (i < HEIGHT - 1) {
+      if (j !== 0 && lines[i + 1][j - 1] === PAPER) num++;
+      if (lines[i + 1][j] === PAPER) num++;
+      if (j < WIDTH & lines[i + 1][j + 1] === PAPER) num++;
+    }
+
+    return num;
+  }
+
+  let temp = 0;
+  do {
+    temp = 0;
+    const next = [];
+    for (let i = 0; i < lines.length; i++) {
+      const row = [];
+      for (let j = 0; j < WIDTH; j++) {
+        if (lines[i][j] === PAPER && countAdjacent(i, j) < 4) {
+          temp++;
+          row.push('x');
+        } else {
+          row.push(lines[i][j]);
+        }
+      }
+      next.push(row);
+    }
+    count += temp;
+    lines = next;
+  } while (temp !== 0)
+
+  log(count);
+}
+
+export default run;
